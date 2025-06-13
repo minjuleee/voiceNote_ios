9a042e8f38f1ae62a0ce08268020734dbcb4e771
minjuleee <minju2138@hansung.ac.kr>
2025-06-13 16:20:48 +0900

    Initial Commit

---
+++ voiceNote/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  voiceNote
+//
+//  Created by 이민주 on 6/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ voiceNote/voiceNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  voiceNoteApp.swift
+//  voiceNote
+//
+//  Created by 이민주 on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct voiceNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
